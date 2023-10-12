c20aa7dbbcbc9488484caf7e985f0f2dd9eaf315
Beksdom <rasulsagitov20110805@gmail.com>
2023-10-12 19:15:09 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,65 @@
+let red = document.querySelectorAll('.red');
+for (let i = 0; i < red.length; i++) {
+    red[i].addEventListener('click', function() {
+        document.body.style.backgroundColor = 'red';
+    });
+}
+let blue = document.querySelectorAll('.blue');
+for (let i = 0; i < blue.length; i++) {
+    blue[i].addEventListener('click', function() {
+        document.body.style.backgroundColor = 'blue';
+    });
+}
+let purple = document.querySelectorAll('.purple');
+for (let i = 0; i < purple.length; i++) {
+    purple[i].addEventListener('click', function() {
+        document.body.style.backgroundColor = 'purple';
+    });
+}
+let pink = document.querySelectorAll('.pink');
+for (let i = 0; i < pink.length; i++) {
+    pink[i].addEventListener('click', function() {
+        document.body.style.backgroundColor = 'pink';
+    });
+}
+let orange = document.querySelectorAll('.orange');
+for (let i = 0; i < orange.length; i++) {
+    orange[i].addEventListener('click', function() {
+        document.body.style.backgroundColor = 'orange';
+    });
+}
+let green = document.querySelectorAll('.green');
+for (let i = 0; i < green.length; i++) {
+    green[i].addEventListener('click', function() {
+        document.body.style.backgroundColor = 'green';
+    });
+}
+let yellow = document.querySelectorAll('.yellow');
+for (let i = 0; i < yellow.length; i++) {
+    yellow[i].addEventListener('click', function() {
+        document.body.style.backgroundColor = 'yellow';
+    });
+}
+let black = document.querySelectorAll('.black');
+for (let i = 0; i < black.length; i++) {
+    black[i].addEventListener('click', function() {
+        document.body.style.backgroundColor = 'black';
+    });
+}
+let brown = document.querySelectorAll('.brown');
+for (let i = 0; i < brown.length; i++) {
+    brown[i].addEventListener('click', function() {
+        document.body.style.backgroundColor = 'brown';
+    });
+}
+let remove = document.querySelectorAll('.remove');
+for (let i = 0; i < remove.length; i++) {
+    remove[i].addEventListener('click', function() {
+        document.body.style.backgroundColor = 'initial';
+    });
+}
+
+//  ______
+// /      \
+//|   ☐☐  |
+//|_🚪_____| ←это домик
